ca9180757718273c5be2cc63a859deb2c3c12b49
cyrus-go <1169792186@qq.com>
2023-07-28 12:10:50 +0800

    feat: add generate hmac

--- h_mac/h_mac.go
+++ h_mac/h_mac.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -53,4 +54,26 @@ func VerifyHMAC(c *gin.Context, key string) (bool, error) {
 	data := bytes.Join(byteList, []byte{})
 
 	return receivedHMAC == CalculateHMAC(data, key), nil
+}
+
+func GenerateHMAC(requestURI, key string, body []byte) (error, string) {
+	var tempStruct map[string]interface{}
+	if err := json.Unmarshal(body, &tempStruct); err != nil {
+		return errors.New("invalid body data"), ""
+	}
+	newBody, err := json.Marshal(tempStruct)
+	if err != nil {
+		return errors.New(fmt.Sprintf("marshal body data failed, err: %v", err)), ""
+	}
+	byteData := []byte(requestURI)
+	for _, b := range newBody {
+		byteData = append(byteData, b)
+	}
+	tempByteData := string(byteData)
+	byteData, err = json.Marshal(tempByteData)
+	if err != nil {
+		return errors.New(fmt.Sprintf("marshal temp data failed, err: %v", err)), ""
+	}
+
+	return nil, CalculateHMAC(byteData, key)
 }
