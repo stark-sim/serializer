41248c493d52d20b9092acfa0fcddbef3b3fe8c3
cyrus-go <1169792186@qq.com>
2023-07-31 18:26:40 +0800

    feat: add paginate types

---
+++ common_types/page.go
@@ -0,0 +1,13 @@
+package common_types
+
+type PaginateReq struct {
+	PageIndex int `form:"page_index" json:"page_index" binding:"required"`
+	PageSize  int `form:"page_size" json:"page_size" binding:"required"`
+}
+
+type PaginateResp struct {
+	PageIndex int         `json:"page_index"`
+	PageSize  int         `json:"page_size"`
+	Total     int64       `json:"total"`
+	List      interface{} `json:"list"`
+}
